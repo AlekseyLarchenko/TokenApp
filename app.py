caf027b75f220184b2984ce4e5f3dc1f83ab4e9e
aleksey <larchenko.aleksey@gmail.com>
2023-10-05 18:28:03 +0300

    first commit

---
+++ app.py
@@ -0,0 +1,12 @@
+gitfrom flask import Flask
+
+app = Flask(__name__)
+
+
+@app.route('/')
+def hello_world():  # put application's code here
+    return 'Hello World!'
+
+
+if __name__ == '__main__':
+    app.run()
